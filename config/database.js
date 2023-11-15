f41cdeecfcf7b0df95db4564ddba5790a23a8a55
Fadhlan54 <muhammadfadhlan0011@gmail.com>
2023-11-15 18:33:42 +0700

    memperbaiki penamaan database pada file database.js

--- config/database.js
+++ config/database.js
@@ -14,7 +14,7 @@ module.exports = {
   development: {
     username: DB_USER,
     password: DB_PASSWORD,
-    database: `${DB_NAME}`,
+    database: `${DB_NAME}_development`,
     host: DB_HOST,
     port: DB_PORT,
     dialect: "postgres",
@@ -22,7 +22,7 @@ module.exports = {
   test: {
     username: DB_USER,
     password: DB_PASSWORD,
-    database: `${DB_NAME}_development`,
+    database: `${DB_NAME}_test`,
     host: DB_HOST,
     port: DB_PORT,
     dialect: "postgres",
@@ -30,7 +30,7 @@ module.exports = {
   production: {
     username: DB_USER,
     password: DB_PASSWORD,
-    database: `${DB_NAME}`,
+    database: `${DB_NAME}_production`,
     host: DB_HOST,
     port: DB_PORT,
     dialect: "postgres",
