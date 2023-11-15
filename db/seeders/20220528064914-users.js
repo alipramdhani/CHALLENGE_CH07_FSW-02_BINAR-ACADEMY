6fb4805d45843ec8e6432ca429fd155645df6cd3
Fadhlan54 <muhammadfadhlan0011@gmail.com>
2023-11-15 17:35:56 +0700

    menambahkan role admin di seeder

--- db/seeders/20220528064914-users.js
+++ db/seeders/20220528064914-users.js
@@ -1,42 +1,47 @@
-'use strict';
+"use strict";
 
 const { Op } = require("sequelize");
 const bcrypt = require("bcryptjs");
 const { Role } = require("../../app/models");
 
-const names = [
-  "Johnny",
-  "Fikri",
-  "Brian",
-  "Ranggawarsita",
-  "Jayabaya",
-]
+const names = ["Johnny", "Fikri", "Brian", "Ranggawarsita", "Jayabaya"];
 
 module.exports = {
-  async up (queryInterface, Sequelize) {
+  async up(queryInterface, Sequelize) {
     const password = "123456";
     const encryptedPassword = bcrypt.hashSync(password, 10);
     const timestamp = new Date();
 
     const role = await Role.findOne({
       where: {
-        name: "CUSTOMER",
-      }
-    })
+        name: "ADMIN",
+      },
+    });
 
-    const users = names.map((name) => ({
+    const usersCustomer = {
+      name: "Jojo",
+      email: "jojo@binar.co.id",
+      encryptedPassword,
+      roleId: 1,
+      createdAt: timestamp,
+      updatedAt: timestamp,
+    };
+
+    let users = names.map((name) => ({
       name,
       email: `${name.toLowerCase()}@binar.co.id`,
       encryptedPassword,
-      roleId: role.id, 
+      roleId: role.id,
       createdAt: timestamp,
       updatedAt: timestamp,
-    }))
+    }));
 
-    await queryInterface.bulkInsert('Users', users, {});
+    users = [...users, usersCustomer];
+
+    await queryInterface.bulkInsert("Users", users, {});
   },
 
-  async down (queryInterface, Sequelize) {
-    await queryInterface.bulkDelete('Users', { name: { [Op.in]: names } }, {});
-  }
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete("Users", { name: { [Op.in]: names } }, {});
+  },
 };
